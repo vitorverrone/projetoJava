f6f49188c27b0e0981e0656ae1ada031fd52f85c
Vitor Verrone <vitorverrone@outlook.com>
2018-10-08 17:06:35 -0300

    Configurações de SQL

---
+++ SQLConn/src/connection/ConnectionFactory.java
@@ -0,0 +1,59 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConnectionFactory {
+    
+    private static String DRIVER = "con.mysql.jdbc.Driver";
+    private static String URL = "jdbc:mysql://localhost:3606";
+    private static String USER = "root";
+    private static String PASS = "password";
+    
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new RuntimeException("Erro na conexão: ", ex);
+        }
+    }
+    
+    public static void closeConnection(Connection con) {
+        if(con != null) {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public static void closeConnection(Connection con, PreparedStatement stmt) {
+        closeConnection(con);
+        try {
+            if(stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs) {
+        closeConnection(con, stmt);
+        try {
+            if(rs != null) {
+                rs.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
